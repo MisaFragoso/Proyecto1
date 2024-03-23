378f92a2f3bfd6323837b0e35455ffd71487e4dc
Marco Landero <54428847+MarcoLJ19@users.noreply.github.com>
2024-03-22 21:43:27 -0600

    Update ClientCris.py
    client-side code updated

--- AlgoritmoCristian/ClientCris.py
+++ AlgoritmoCristian/ClientCris.py
@@ -2,32 +2,31 @@ import socket
 import time
 import datetime
 
-def stringToTime(cadena):
-    format= '%Y%m%d %H:%M:%f'
-    horaCadena=datetime.datetime.strptime(cadena,format)
-    return horaCadena
+def stringToTime(string):
+    format = '%Y%m%d %H:%M:%f'
+    timeString = datetime.datetime.strptime(string,format)
+    return timeString
 
 ServerIP="localhost"
 ServerPort= 9099
-#Apertura del socket
+#creación del socket
 clientSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 start=time.time()
 #Solicitar conexion con el servidor
 clientSocket.connect((ServerIP,ServerPort))
 #Recibir del servidor
-horaCadena=clientSocket.recv(4096).decode()
+timeString = clientSocket.recv(4096).decode()
 
-final=time.time()
-time=final-start
+final = time.time()
+time = final-start
 
-hora=stringToTime(horaCadena)
-print("El  tiempoTotal de ida y vuelta fue:", time)
+hora = stringToTime(timeString)
+print("Total time (ida y vuelta): ", time)
 
 halfTime= time/2
 
-print("Tiempo total de ida", halfTime)
-print("Hora servidor", horaCadena)
-print("La hora exacta es" , hora+datetime.timedelta(seconds=halfTime))
+print("Total time: ", halfTime)
+print("Server time: ", timeString)
+print("Exact time: " , hora + datetime.timedelta(seconds=halfTime))
 
 clientSocket.close()
